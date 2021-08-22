99b705e53f4db6853c754a13a22e2416b5900ffc
vetrivel-light <vetrivelkalidass10700@gmail.com>
2021-08-22 15:34:14 +0530

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/van-restaurant'
-};
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '958641554915809',
+        clientSecret: 'ab544d7dec2bd00e6b9fe04abba6ec9d'
+    }
+}
--- models/user.js
+++ models/user.js
@@ -11,6 +11,7 @@ var user = new Schema({
         type: String,
         default: ''
     },
+    facebookId: String,
     admin: {
         type: Boolean,
         default: false
